9e4fe12048f8643f93cbdce569646b5ceda48484
LucasPLTC <lucas.carvalho.pltc@gmail.com>
2018-03-13 12:13:14 -0300

    """Movimentação"""
    Gambiarra, funciona bem mas ñ vai fica assim.

--- Assets/Scripts/MovimentoPlayer.cs
+++ Assets/Scripts/MovimentoPlayer.cs
@@ -19,14 +19,29 @@ public class MovimentoPlayer : MonoBehaviour {
     float gravidade;
     float forcaPulo;
     private Rigidbody rb;
+
+    public float speed2 = 10.0F;
 	// Use this for initialization
 	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+
+
         player = GetComponent<CharacterController>();
         rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        float translation = Input.GetAxis("Vertical") * speed2;
+        float straffe = Input.GetAxis("Horizontal") * speed2;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+
+        
         moveFB = Input.GetAxis("Vertical");
         moveLR = Input.GetAxis("Horizontal");
 
